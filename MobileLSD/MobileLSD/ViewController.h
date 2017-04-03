1823ec311b49125bc517a9b8c7a252fec42f86a4
AJ Bruce <ddbruce@Dan-Bruces-MacBook-Pro.local>
2017-04-03 11:26:47 +0300

    mobileSLAM implementation

---
+++ MobileLSD/MobileLSD/ViewController.h
@@ -0,0 +1,30 @@
+//
+//  ViewController.h
+//  MobileLSD
+//
+//  Created by Guanhang Wu on 11/24/16.
+//  Copyright © 2016 Guanhang Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//#import <opencv2/highgui/ios.h>
+//#import <opencv2/highgui/cap_ios.h>
+//#import <opencv2/opencv.hpp>
+//#import <opencv2/imgcodecs/ios.h>
+#import <opencv2/videoio/cap_ios.h>
+
+using namespace cv;
+
+@interface ViewController : UIViewController<CvVideoCameraDelegate>
+{
+ 
+    CvVideoCamera *videoCamers;
+}
+
+
+
+@property (nonatomic, retain) CvVideoCamera *videoCamera;
+
+
+@end
+
